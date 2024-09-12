1dd27ad4ae49fe042c55f05e851d6391d64d7aac
Mini <minnnidev@gmail.com>
2024-09-13 01:50:52 +0900

    feat: 핍 미리보기 sheet 추가

---
+++ PeepIt/PeepIt/Components/ClearBackgroundView.swift
@@ -0,0 +1,36 @@
+//
+//  ClearModalBackgroundView.swift
+//  PeepIt
+//
+//  Created by 김민 on 9/13/24.
+//
+
+import SwiftUI
+import UIKit
+
+struct ClearBackgroundView: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView()
+        
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) { }
+}
+
+struct ClearBackgroundViewModifier: ViewModifier {
+
+    func body(content: Content) -> some View {
+        if #available(iOS 16.4, *) {
+            content
+                .presentationBackground(.clear)
+        } else {
+            content
+                .background(ClearBackgroundView())
+        }
+    }
+}
---
+++ PeepIt/PeepIt/Features/PeepPreview/PeepPreviewModalView.swift
@@ -0,0 +1,18 @@
+//
+//  PeepPreviewModalView.swift
+//  PeepIt
+//
+//  Created by 김민 on 9/13/24.
+//
+
+import SwiftUI
+
+struct PeepPreviewModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeepPreviewModalView()
+}
