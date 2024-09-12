f2cae4816a2ca92ae953f9b182a6f9b65e195f27
Mini <minnnidev@gmail.com>
2024-09-12 22:34:02 +0900

    setting: 프로젝트 생성

---
+++ PeepIt/PeepIt/PeepItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeepItApp.swift
+//  PeepIt
+//
+//  Created by 김민 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PeepItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
