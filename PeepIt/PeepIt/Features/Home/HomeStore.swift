68b553f632c5e0b7b76334106e3eaa71d5b55cf0
Mini <minnnidev@gmail.com>
2024-09-14 19:45:42 +0900

    feat: 미리보기 시트 높이에 따라 버튼 위치 함께 이동

--- PeepIt/PeepIt/Features/Home/HomeStore.swift
+++ PeepIt/PeepIt/Features/Home/HomeStore.swift
@@ -5,6 +5,7 @@
 //  Created by 김민 on 9/12/24.
 //
 
+import SwiftUI
 import ComposableArchitecture
 
 @Reducer
@@ -12,10 +13,20 @@ struct HomeStore {
     
     @ObservableState
     struct State {
-
+        var sheetType: SheetType = .fold
     }
 
     enum Action {
+        case setSheet(isExpanded: Bool)
+    }
 
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case let .setSheet(isExpanded):
+                state.sheetType = isExpanded ? .unfold : .fold
+                return .none
+            }
+        }
     }
 }
--- PeepIt/PeepIt/Features/Home/HomeView.swift
+++ PeepIt/PeepIt/Features/Home/HomeView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 import ComposableArchitecture
 
+enum SheetType {
+    case fold, unfold
+
+    var height: CGFloat {
+        switch self {
+        case .fold:
+            return CGFloat(60)
+        case .unfold:
+            return CGFloat(500)
+        }
+    }
+}
+
 struct ContentView: View {
     let store: StoreOf<HomeStore>
 
@@ -34,6 +47,9 @@ struct ContentView: View {
                     uploadPeepButton
                 }
                 .padding(.horizontal, 17)
+                .padding(
+                    .bottom, store.state.sheetType.height + 17
+                )
             }
         }
         .sheet(isPresented: .constant(true)) {
@@ -41,8 +57,18 @@ struct ContentView: View {
                 .ignoresSafeArea()
                 .frame(maxWidth: .infinity)
                 .clearModalBackground()
-                .presentationDetents([.height(60), .medium])
+                .presentationDetents([.height(60), .height(500)])
                 .interactiveDismissDisabled()
+                .background(GeometryReader { geometry in
+                    Color.clear
+                        .onChange(of: geometry.size.height) { newHeight in
+                            if newHeight == SheetType.fold.height {
+                                store.send(.setSheet(isExpanded: false))
+                            } else if newHeight == SheetType.unfold.height {
+                                store.send(.setSheet(isExpanded: true))
+                            }
+                        }
+                })
         }
     }
 }
