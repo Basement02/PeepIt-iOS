dcb9bf2407a382f963030227a4377d4383f710f5
Mini <minnnidev@gmail.com>
2024-09-22 17:45:56 +0900

    add: screen size constant 추가

---
+++ PeepIt/PeepIt/Utils/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  PeepIt
+//
+//  Created by 김민 on 9/22/24.
+//
+
+import UIKit
+
+struct Constant {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
