a70334dcce950d00ced94f99e6ed87ec7a4f8cd2
Mini <minnnidev@gmail.com>
2024-09-24 00:16:52 +0900

    add: 유저 모델에 동네 추가

--- PeepIt/PeepIt/Models/UserProfile.swift
+++ PeepIt/PeepIt/Models/UserProfile.swift
@@ -11,6 +11,7 @@ struct UserProfile: Equatable, Hashable {
     let id: String
     let nickname: String
     let profileImage: String
+    let town: String
 }
 
 extension UserProfile {
@@ -19,7 +20,8 @@ extension UserProfile {
         return .init(
             id: "user1_id",
             nickname: "user1_nickname",
-            profileImage: "https://images.unsplash.com/photo-1726268591596-07bb5a41cf75?q=80&w=2350&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            profileImage: "https://images.unsplash.com/photo-1726268591596-07bb5a41cf75?q=80&w=2350&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            town: "신당동"
         )
     }
 
@@ -27,7 +29,8 @@ extension UserProfile {
         return .init(
             id: "user2_id",
             nickname: "user2_nickname",
-            profileImage: "https://images.unsplash.com/photo-1726268591596-07bb5a41cf75?q=80&w=2350&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            profileImage: "https://images.unsplash.com/photo-1726268591596-07bb5a41cf75?q=80&w=2350&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            town: "화양동"
         )
     }
 }
