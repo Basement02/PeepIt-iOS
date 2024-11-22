cb9e52e9adfc2b78f8ba521765c64b767d9ed9f4
Mini <minnnidev@gmail.com>
2024-11-22 13:39:11 +0900

    feat: 서비스 공지 상세 모달 GUI

---
+++ PeepIt/PeepIt/Features/Announce/AnnounceModal.swift
@@ -0,0 +1,81 @@
+//
+//  AnnounceModal.swift
+//  PeepIt
+//
+//  Created by 김민 on 11/22/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct AnnounceModal: View {
+    let store: StoreOf<AnnounceStore>
+
+    var body: some View {
+        VStack(spacing: 0) {
+            PeepItNavigationBar(
+                title: "서비스 소식",
+                trailing: dismissButton
+            )
+
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 0) {
+                    title
+                        .padding(.top, 39.adjustedH)
+                        .padding(.bottom, 30.adjustedH)
+
+                    Text(store.selectedAnnounce?.content ?? "")
+                        .pretendard(.body05)
+                        .padding(.bottom, 30.adjustedH)
+
+                    if let _ = store.selectedAnnounce?.image {
+                        image
+                    }
+                }
+            }
+            .frame(width: 335)
+
+            Spacer()
+        }
+        .background(Color.base)
+        .toolbar(.hidden, for: .navigationBar)
+    }
+
+    private var dismissButton: some View {
+        DismissButton {
+            store.send(.closeSheet)
+        }
+    }
+
+    private var title: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(store.selectedAnnounce?.category ?? "")
+                .pretendard(.caption01)
+                .foregroundStyle(Color.coreLime)
+
+            Text(store.selectedAnnounce?.title ?? "")
+                .pretendard(.title02)
+
+            Text(store.selectedAnnounce?.date ?? "")
+                .pretendard(.caption02)
+                .foregroundStyle(Color.nonOp)
+
+            Rectangle()
+                .fill(Color.op)
+                .frame(height: 0.5)
+        }
+    }
+
+    // TODO:
+    private var image: some View {
+        Rectangle()
+            .fill(Color.gray900)
+            .frame(width: 335, height: 150)
+    }
+}
+
+#Preview {
+    AnnounceModal(
+        store: .init(initialState: AnnounceStore.State()) { AnnounceStore() }
+    )
+}
