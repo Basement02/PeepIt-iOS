870f9d9cba89d9da22b786fbc1e2afff2a2a1470
Mini <minnnidev@gmail.com>
2024-11-24 21:55:44 +0900

    feat: 알림 설정 GUI

---
+++ PeepIt/PeepIt/Features/Setting/Notification/NotificationSettingStore.swift
@@ -0,0 +1,84 @@
+//
+//  NotificationSettingStore.swift
+//  PeepIt
+//
+//  Created by 김민 on 11/24/24.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct NotificationSettingStore {
+
+    @ObservableState
+    struct State: Equatable {
+
+        /// 전체 푸쉬 알림
+        var alarmIsOn = false
+
+        /// 핍 알림
+        var peepNotiSettings: [PeepNotiType: Bool] = [
+            .newChat: false,
+            .newReaction: false,
+            .pickedHot: false,
+            .recommendPeep: false
+        ]
+
+        /// 서비스 알림
+        var serviceNotiSettings: [ServiceNotiType: Bool] = [
+            .newAnnounce: false,
+            .others: false,
+            .marketing: false
+        ]
+
+        /// 핍 알림 enum
+        enum PeepNotiType: String, CaseIterable {
+            case newChat = "새로운 채팅"
+            case newReaction = "새로운 반응"
+            case pickedHot = "인기 핍 선정 알림"
+            case recommendPeep = "인기 핍 추천"
+        }
+
+        /// 서비스 알림 enum
+        enum ServiceNotiType: String, CaseIterable {
+            case newAnnounce = "새로운 소식"
+            case others = "기타(신고 등) 알림"
+            case marketing = "마케팅 알림"
+        }
+    }
+
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case peepNotiSettingTapped(type: State.PeepNotiType)
+        case serviceNotiSettingTapeed(type: State.ServiceNotiType)
+    }
+
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+
+        Reduce { state, action in
+            switch action {
+
+            case .binding(\.alarmIsOn):
+                guard !state.alarmIsOn else { return .none }
+
+                state.peepNotiSettings = state.peepNotiSettings.mapValues { _ in false }
+                state.serviceNotiSettings = state.serviceNotiSettings.mapValues { _ in false }
+
+                return .none
+
+            case let .peepNotiSettingTapped(type):
+                state.peepNotiSettings[type]?.toggle()
+                return .none
+
+            case let .serviceNotiSettingTapeed(type):
+                state.serviceNotiSettings[type]?.toggle()
+                return .none
+
+            default:
+                return .none
+            }
+        }
+    }
+}
---
+++ PeepIt/PeepIt/Features/Setting/Notification/NotificationSettingView.swift
@@ -0,0 +1,134 @@
+//
+//  NotificationSettingView.swift
+//  PeepIt
+//
+//  Created by 김민 on 11/24/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct NotificationSettingView: View {
+    @Perception.Bindable var store: StoreOf<NotificationSettingStore>
+
+    var body: some View {
+        VStack(spacing: 0) {
+            PeepItNavigationBar(
+                leading: backButton,
+                title: "알림 설정"
+            )
+            .padding(.bottom, 39.adjustedH)
+
+            VStack(spacing: 37.adjustedH) {
+                topButton
+
+                peepNotiSettingList
+
+                serviceNotiSettingList
+            }
+            .padding(.horizontal, 29)
+
+            Spacer()
+        }
+        .background(Color.base)
+        .ignoresSafeArea(.all, edges: .bottom)
+    }
+
+    private var backButton: some View {
+        BackButton {
+            // TODO:
+        }
+    }
+
+    private var topButton: some View {
+        HStack {
+            Text("푸쉬 알림")
+                .pretendard(.foodnote)
+
+            Toggle("", isOn: $store.alarmIsOn)
+                .tint(Color.coreLime)
+        }
+        .padding(.vertical, 11)
+        .padding(.horizontal, 20)
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.gray700)
+        )
+    }
+
+    private func header(title: String) -> some View {
+        HStack {
+            Text(title)
+                .pretendard(.caption01)
+                .foregroundStyle(Color.gray400)
+            Spacer()
+        }
+    }
+
+    private var peepNotiSettingList: some View {
+        VStack(spacing: 24) {
+            header(title: "핍 알림")
+
+            VStack(spacing: 25) {
+                ForEach(
+                    NotificationSettingStore.State.PeepNotiType.allCases,
+                    id: \.self
+                ) { type in
+                    ToggleMenu(
+                        title: type.rawValue,
+                        isOn: store.peepNotiSettings[type] ?? false
+                    )
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        store.send(.peepNotiSettingTapped(type: type))
+                    }
+                }
+            }
+        }
+    }
+
+    private var serviceNotiSettingList: some View {
+        VStack(spacing: 24) {
+            header(title: "서비스 알림")
+
+            VStack(spacing: 25) {
+                ForEach(
+                    NotificationSettingStore.State.ServiceNotiType.allCases,
+                    id: \.self
+                ) { type in
+                    ToggleMenu(
+                        title: type.rawValue,
+                        isOn: store.serviceNotiSettings[type] ?? false
+                    )
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        store.send(.serviceNotiSettingTapeed(type: type))
+                    }
+                }
+            }
+        }
+    }
+}
+
+fileprivate struct ToggleMenu: View {
+    let title: String
+    let isOn: Bool
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .pretendard(.body04)
+            Spacer()
+            Image(isOn ? "CheckY" : "CheckN")
+                .frame(width: 25.2, height: 25.2)
+        }
+    }
+}
+
+#Preview {
+    NotificationSettingView(
+        store: .init(initialState: NotificationSettingStore.State()) {
+            NotificationSettingStore()
+        }
+    )
+}
