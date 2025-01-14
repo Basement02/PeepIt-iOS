42fc839ba817c1fcf219d12cef80cb6e6a034370
Mini <minnnidev@gmail.com>
2025-01-14 22:59:24 +0900

    feat: id 속성 추가

--- PeepIt/PeepIt/Models/Chat.swift
+++ PeepIt/PeepIt/Models/Chat.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct Chat: Equatable, Hashable {
+    let id: String
     let user: UserProfile
     let message: String
     let sendTime: String
@@ -18,6 +19,7 @@ extension Chat {
 
     static var chatStub1: Chat {
         return .init(
+            id: "1",
             user: .stubUser1,
             message: "안녕하세요!",
             sendTime: "5분 전",
@@ -27,6 +29,7 @@ extension Chat {
 
     static var chatStub2: Chat {
         return .init(
+            id: "2",
             user: .stubUser2,
             message: "안녕하세요!",
             sendTime: "5분 전",
