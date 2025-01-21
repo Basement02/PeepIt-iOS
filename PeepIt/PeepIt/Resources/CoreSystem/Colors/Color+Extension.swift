2eb4dd6926325e2a17432a0012cdf6c231d5e2b1
Mini <minnnidev@gmail.com>
2025-01-21 16:30:23 +0900

    add: 컬러 추가

--- PeepIt/PeepIt/Resources/CoreSystem/Colors/Color+Extension.swift
+++ PeepIt/PeepIt/Resources/CoreSystem/Colors/Color+Extension.swift
@@ -19,6 +19,7 @@ extension Color {
     static let coreLime: Color = .init(hex: 0xCEFF00)
     static let coreRed: Color = .init(hex: 0xFF5959)
     static let coreLimeDOp: Color = .init(hex: 0x6B8400, alpha: 0.5)
+    static let coreLimeOp: Color = Color.coreLime.opacity(0.5)
 
     /// Element Basic
     static let gray900: Color = .init(hex: 0x2F2F2F)
