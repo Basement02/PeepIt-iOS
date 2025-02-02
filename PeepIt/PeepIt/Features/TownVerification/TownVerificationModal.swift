ff368f05185705bbe044b2d6064f08b7c3adb8b2
Mini <minnnidev@gmail.com>
2025-02-03 01:04:12 +0900

    feat: 동네 등록 GUI

---
+++ PeepIt/PeepIt/Features/TownVerification/TownVerificationModal.swift
@@ -0,0 +1,74 @@
+//
+//  TownRegisterModalView.swift
+//  PeepIt
+//
+//  Created by 김민 on 2/3/25.
+//
+
+import SwiftUI
+
+struct TownRegisterModalView: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Color.clear
+
+            VStack {
+                Spacer()
+
+                BackdropBlurView(bgColor: .base, radius: 4)
+                    .roundedCorner(20, corners: [.topLeft, .topRight])
+                    .overlay {
+                        VStack(spacing: 0) {
+                            RoundedRectangle(cornerRadius: 100)
+                                .fill(Color.gray600)
+                                .frame(width: 60, height: 5)
+                                .padding(.top, 10)
+                                .padding(.bottom, 26)
+
+                            Text("현재 설정된 동네")
+                                .pretendard(.foodnote)
+                                .foregroundStyle(Color.gray300)
+                                .padding(.bottom, 26)
+
+                            HStack(spacing: 0) {
+                                Text("OO구 OO동")
+                                    .pretendard(.title03)
+                            }
+                            .frame(width: 181, height: 52)
+                            .padding(.bottom, 26)
+
+                            ZStack {
+                                HomeMapView()
+                                BackMapLayer.teriary()
+                                Circle()
+                                    .fill(Color.coreLimeOp)
+                                    .frame(width: 127, height: 126)
+                                Circle()
+                                    .stroke(Color.coreLimeClick, lineWidth: 2)
+                                    .frame(width: 127, height: 126)
+                            }
+                            .frame(width: 327, height: 350)
+                            .clipShape(RoundedRectangle(cornerRadius: 14))
+                            .padding(.bottom, 67)
+
+                            Text("새로운 동네를 인증하시겠어요?")
+                                .pretendard(.body04)
+                                .padding(.bottom, 20)
+
+                            Text("동네 등록하기")
+                                .mainGrayButtonStyle()
+
+                            Spacer()
+                        }
+                    }
+                    .frame(height: 775)
+            }
+            .frame(height: Constant.screenHeight)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    TownRegisterModalView()
+}
