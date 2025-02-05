4c3686752b5e7ef83beb3a658811ec6cc1c36cce
Mini <minnnidev@gmail.com>
2025-02-05 22:04:54 +0900

    add: safe area 구하기

--- PeepIt/PeepIt/Utils/Constant/Constant.swift
+++ PeepIt/PeepIt/Utils/Constant/Constant.swift
@@ -10,4 +10,14 @@ import UIKit
 struct Constant {
     static let screenWidth = UIScreen.main.bounds.width
     static let screenHeight = UIScreen.main.bounds.height
+
+    static var safeAreaInsets: UIEdgeInsets {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .compactMap { ($0 as? UIWindowScene)?.windows.first { $0.isKeyWindow } }
+            .first
+        return keyWindow?.safeAreaInsets ?? UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+    static var safeAreaTop: CGFloat { safeAreaInsets.top }
+    static var safeAreaBottom: CGFloat { safeAreaInsets.bottom }
 }
