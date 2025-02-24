17a8c9d96e1643dde5d41660e63bef26bc888f9c
Mini <minnnidev@gmail.com>
2025-02-24 21:27:29 +0900

    feat: white 케이스 버튼 인터렉션

--- PeepIt/PeepIt/Features/TownVerification/TownVerificationView.swift
+++ PeepIt/PeepIt/Features/TownVerification/TownVerificationView.swift
@@ -100,24 +100,27 @@ struct TownVerificationView: View {
     }
 
     private var toCurrentLocationButton: some View {
-        RoundedRectangle(cornerRadius: 100)
-            .fill(Color.white)
-            .frame(width: 194, height: 38)
-            .overlay(alignment: .leading) {
-                HStack(spacing: 3) {
-                    Image("IconDirection")
-                    Text("현재 위치로 돌아가기")
-                        .pretendard(.body04)
-                        .foregroundStyle(Color.base)
-                }
-                .padding(.leading, 23)
+        Button {
+
+        } label: {
+            HStack(spacing: 3) {
+                Image("IconDirection")
+                Text("현재 위치로 돌아가기")
+                    .pretendard(.body04)
+                    .foregroundStyle(Color.base)
+                Spacer()
             }
-            .shadow(
-                color: Color(hex: 0x202020, alpha: 0.15),
-                radius: 5,
-                x: 0,
-                y: 4
-            )
+            .padding(.leading, 23)
+            .frame(width: 194, height: 38)
+        }
+        .buttonStyle(PressableButtonStyle(colorStyle: .white))
+        .shadow(
+            color: Color(hex: 0x202020, alpha: 0.15),
+            radius: 5,
+            x: 0,
+            y: 4
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 100))
     }
 
     private var verifyButton: some View {
