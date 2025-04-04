4c4672898bf8dfaa87a27e010e3dbcc16f678df2
Mini <minnnidev@gmail.com>
2025-04-04 23:42:17 +0900

    feat: 현재 위치 파악 후 나타내는 로직

--- PeepIt/PeepIt/Features/Home/HomeMapView.swift
+++ PeepIt/PeepIt/Features/Home/HomeMapView.swift
@@ -7,23 +7,70 @@
 
 import SwiftUI
 import MapKit
-
+import CoreLocation
 
 struct HomeMapView: UIViewRepresentable {
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
 
         mapView.overrideUserInterfaceStyle = .light
-        // TODO: - 추후 수정
-        let coordinate = CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780)
-        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.showsUserLocation = true
 
-        mapView.setRegion(region, animated: false) 
+        context.coordinator.mapView = mapView
+        context.coordinator.setupLocation()
 
         return mapView
     }
 
     func updateUIView(_ uiView: MKMapView, context: Context) { }
+
+    class Coordinator: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
+        var parent: HomeMapView
+        var mapView: MKMapView?
+        
+        private let locationManager = CLLocationManager()
+        private var hasCentered = false
+
+        init(_ parent: HomeMapView) {
+            self.parent = parent
+            super.init()
+            locationManager.delegate = self
+        }
+
+        func setupLocation() {
+            switch locationManager.authorizationStatus {
+
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+
+            case .authorizedWhenInUse, .authorizedAlways:
+                locationManager.startUpdatingLocation()
+
+            default:
+                break
+            }
+        }
+
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard !hasCentered, let location = locations.last else { return }
+
+            let region = MKCoordinateRegion(
+                center: location.coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
+
+            mapView?.setRegion(region, animated: true)
+            hasCentered = true
+        }
+
+        func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+            setupLocation()
+        }
+    }
 }
