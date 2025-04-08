65e70d3d5a4d23192a7632ac6a700e7894d6704e
Mini <minnnidev@gmail.com>
2025-04-08 16:34:51 +0900

    chore: 문자열로 좌표 매핑

--- PeepIt/PeepIt/Network/Town/API/TownAPIClient.swift
+++ PeepIt/PeepIt/Network/Town/API/TownAPIClient.swift
@@ -16,7 +16,7 @@ extension TownAPIClient: DependencyKey {
 
     static let liveValue: TownAPIClient = TownAPIClient(
         getCurrentLegalCode: { coord in
-            let requestDto: LegalCodeRequestDto = .init(x: coord.x, y: coord.y)
+            let requestDto: LegalCodeRequestDto = .init(x: String(coord.x), y: String(coord.y))
             let requestAPI: TownAPI = .getLegalCode(requestDto)
             let response: LegalCodeResponseDto = try await APIFetcher.shared.fetch(of: requestAPI)
             return response.toLegalCode()
