2ebc0046e90155427337e52603c3a18c6c442f47
Mini <minnnidev@gmail.com>
2025-04-08 16:35:13 +0900

    chore: 앱 진입점 변경

--- PeepIt/PeepIt/Features/Root/RootStore.swift
+++ PeepIt/PeepIt/Features/Root/RootStore.swift
@@ -59,7 +59,7 @@ struct RootStore {
         var path = StackState<Path.State>()
 
         var isLoading = true
-        var authState = AuthState.unAuthorized
+        var authState = AuthState.authorized
 
         var login = LoginStore.State()
         var home = HomeStore.State()
