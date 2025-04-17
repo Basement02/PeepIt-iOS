c40eb088cb7c165f7c0be8cbc891ce69c98d4767
Mini <minnnidev@gmail.com>
2025-04-17 19:44:36 +0900

    feat: 체크 속성 입력 필드 컴포넌트 생성

---
+++ PeepIt/PeepIt/Components/Views/EnterFieldWithCheck.swift
@@ -0,0 +1,78 @@
+//
+//  EnterFieldWithCheck.swift
+//  PeepIt
+//
+//  Created by 김민 on 4/17/25.
+//
+
+import SwiftUI
+
+struct EnterFieldWithCheck: View {
+    let obj: String
+
+    @Binding var text: String
+    @Binding var validState: EnterState
+    @Binding var guideMessage: String
+
+    @FocusState private var isFocused: Bool
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(obj)
+                .pretendard(.caption01)
+                .padding(.bottom, 20)
+
+            HStack {
+                TextField(
+                    "\(obj.withObjectParticle) 입력해 주세요.",
+                    text: $text
+                )
+                .focused($isFocused)
+                .pretendard(.body02)
+                .tint(
+                    validState == .base ?
+                    Color.coreLime : Color.white
+                )
+                .frame(height: 29.4)
+
+                Spacer()
+
+                Group {
+                    switch validState {
+                    case .base:
+                        Image("IconOKsign")
+                    case .completed:
+                        Image("IconOKsignLime")
+                    case .error:
+                        Image("IconNOsign")
+                    }
+                }
+            }
+            .padding(.bottom, 10)
+
+            Rectangle()
+                .frame(height: 1)
+                .foregroundStyle(validState.foregroundColor)
+                .padding(.bottom, 10)
+
+            Text(guideMessage)
+                .pretendard(.caption03)
+                .foregroundStyle(validState.foregroundColor)
+        }
+        .onAppear {
+            Task {
+                try? await Task.sleep(for: .seconds(0.5))
+                isFocused = true
+            }
+        }
+    }
+}
+
+#Preview {
+    EnterFieldWithCheck(
+        obj: "닉네임",
+        text: .constant(""),
+        validState: .constant(.base),
+        guideMessage: .constant("가이드 문구")
+    )
+}
